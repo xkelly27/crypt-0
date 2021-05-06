14b45452745b3e2d79ed4aa9299697c8d3a87952
xkelly27 <xaviernpkelly@gmail.com>
2021-05-06 00:25:51 -0600

    first commit

---
+++ c_backtest.py
@@ -0,0 +1,27 @@
+import c_data as CD
+
+# -------- in progress --------
+
+class Backtest:
+    def __init__(self, ticker):
+        self.data = CD.Data(ticker).get_data()
+
+    def check_data(self):
+        return self.data
+
+    def start_of_period_sim(self, initial_investment=1):
+        # simulate start of period of data input, add observation at t + 1
+
+
+    # need f(x) s.t midpoint is found
+    def binary_search(self, array, value, slice=2, offset=0):
+        mid = array.length / slice
+
+        pass
+
+
+
+
+
+
+print(Backtest("ETH-USD").check_data())
