dba7c28bb70ae2b17e4f1b1f2ad1aeb97209f597
xkelly27 <xaviernpkelly@gmail.com>
2021-05-06 00:26:50 -0600

    added backtest debug

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import c_data as C
-import datetime
-# yfinance refresher: period ex: 'ytd'
-#                     interval ex: '1m'
+import c_data as CD
+import c_backtest as CB
 
-crypto_list = ["BTC-USD", "ETH-USD", "DOGE-USD"]
+# valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+# valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
 
-C.Data(crypto_list[0]).plot("hist")
+crypto_list = ["BTC-USD", "ETH-USD", "DOGE"
+                                     "-USD"]
+
+CD.Data(crypto_list[2]).plot("hist")
+
 
 
 
