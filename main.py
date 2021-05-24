dd9826d6d9aca8c6704c6900d8b58f6f1a4e9160
xkelly27 <xaviernpkelly@gmail.com>
2021-05-23 21:13:58 -0600

    updated main

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 # meta
 from src import data, backtest, plot, stats
 
+crypto_list = ["BTC-USD", "ETH-USD", "DOGE-USD"]
+periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y",
+           "2y", "5y", "10y", "ytd", "max"]
+intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m",
+             "1h", "1d", "5d", "1wk", "1mo", "3mo"]
 
-def test():
-    periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y",
-               "2y", "5y", "10y", "ytd", "max"]
-    intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m",
-                 "1h", "1d", "5d", "1wk", "1mo", "3mo"]
 
-    crypto_list = ["BTC-USD", "ETH-USD", "DOGE-USD"]
+main_setup = {"period": "1mo",
+              "interval": "60m"}
 
-    output = []
-    for i in crypto_list:
-        latest = str(data.Data(i).get_latest())
-        output.append(i + " latest -> " + latest)
-    print(output)
 
-test()
+def main():
+    output_dict = {}
+    for crypto in crypto_list:
+        df = data.Data(crypto, period=main_setup["period"],
+                       interval=main_setup["interval"])
+        bds = stats.norm_dict(data=df)["bounds"]
+        output_dict[crypto] = stats.is_latest_extreme(data=df, bounds=bds)
+    return output_dict
+
+if __name__=="__main__":
+    crypto_data = main()
+    for key, val in crypto_data.items():
+        if val=="True":
+            print(key, val)
+        else:
+            print("No Moves with selected parameters")
