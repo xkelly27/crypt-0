181c097865c593b72c6de32b44ed5786aad1ad33
xkelly27 <xaviernpkelly@gmail.com>
2021-05-25 23:38:01 -0600

    added json for inputs

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 # meta
 from src import data, backtest, plot, stats
+import json
 
+# -------------------- see input_data folder for acceptable inputs --------------------
 
-# -------------------- acceptable inputs below --------------------
-
-crypto_list = ["BTC-USD", "ETH-USD", "DOGE-USD"]
-periods = ["1d", "5d", "1mo", "3mo", "6mo", "1y",
-           "2y", "5y", "10y", "ytd", "max"]
-intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m",
-             "1h", "1d", "5d", "1wk", "1mo", "3mo"]
-main_setup = {"period": "1mo",
-              "interval": "60m"}
-
+# todo - fix with... open for intervals and periods
 
 def main():
     output_dict = {}
-    for crypto in crypto_list:
-        df = data.Data(crypto, period=main_setup["period"],
-                       interval=main_setup["interval"])
-        bds = stats.norm_dict(data=df)["bounds"]
-        output_dict[crypto] = stats.is_latest_extreme(data=df, bounds=bds)
+
+    with open('/input_data/tickers.json') as tickers:
+        t = json.load(tickers)
+        for crypto in t.values():
+            df = data.Data(crypto, period=main_setup["period"],
+                           interval=main_setup["interval"])
+            bds = stats.norm_dict(data=df)["bounds"]
+            output_dict[crypto] = stats.is_latest_extreme(data=df, bounds=bds)
+
     return output_dict
 
 
